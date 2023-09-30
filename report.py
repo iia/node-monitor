c7b82540b0e99eea123618e3f3fc4433cacd4969
Ishraq Ibne Ashraf <ishraq.i.ashraf@gmail.com>
2023-09-30 12:14:00 +0200

    Get rid of interval

--- report.py
+++ report.py
@@ -4,45 +4,37 @@ import time
 
 FILE_NODES = "./nodes.json"
 
-while True:
-    report = None
+report = None
 
-    with open(FILE_NODES, "r+") as f:
-        report = json.load(f)
+with open(FILE_NODES, "r+") as f:
+    report = json.load(f)
 
-        if "interval" not in report:
-            report["interval"] = 10
+    if "runs" not in report:
+        report["runs"] = 1
+    else:
+        report["runs"] += 1
 
-        if "runs" not in report:
-            report["runs"] = 1
+    for node in report["nodes"]:
+        print("<I> Checking node: " + node["address"])
+
+        if report["runs"] == 1:
+            node["times_available"] = 0
+
+        p = os.popen("ping -c 2 " + node["address"])
+
+        p_out = p.read()
+
+        if "time=" in p_out:
+            print("<I> Node available: " + node["address"] + "\n")
+
+            node["times_available"] += 1
         else:
-            report["runs"] += 1
+            print("<I> Node unavailable: " + node["address"] + "\n")
 
-        for node in report["nodes"]:
-            print("<I> Checking node: " + node["address"])
+        node["availability"] = (node["times_available"] / report["runs"]) * 100
 
-            if report["runs"] == 1:
-                node["times_available"] = 0
+with open(FILE_NODES, "w+") as f:
+    print("<I> Writing report...\n")
 
-            p = os.popen("ping -c 2 " + node["address"])
-
-            p_out = p.read()
-
-            if "time=" in p_out:
-                print("<I> Node available: " + node["address"] + "\n")
-
-                node["times_available"] += 1
-            else:
-                print("<I> Node unavailable: " + node["address"] + "\n")
-
-            node["availability"] = (node["times_available"] / report["runs"]) * 100
-
-    with open(FILE_NODES, "w+") as f:
-        print("<I> Writing report...\n")
-
-        f.write("")
-        f.write(json.dumps(report, indent = 4))
-
-    print("<I> Sleeping: " + str(report["interval"]) + " seconds\n")
-
-    time.sleep(report["interval"])
+    f.write("")
+    f.write(json.dumps(report, indent = 4))
